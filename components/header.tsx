b45f4310743311e674fe5889e3213cfad8354d30
harryyking <arthurharry06@gmail.com>
2025-02-07 11:58:56 +0000

    nav

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,6 @@ export function Header() {
 
   return (
     <header
-      className="bg-transparent z-10"
     >
       <div className="max-w-7xl mx-auto px-4 h-16 flex items-center justify-between">
         <div className="flex items-center gap-8">
