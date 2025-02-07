8a563a01c3860c67be5eeadd85a7127af99548ae
harryyking <arthurharry06@gmail.com>
2025-02-07 11:06:17 +0000

    image edit

--- app/page.tsx
+++ app/page.tsx
@@ -33,12 +33,12 @@ export default function Home() {
         <div 
           className="relative h-[600px] flex items-center justify-center overflow-hidden"
           style={{
-            backgroundImage: "url(https://images.pexels.com/photos/1181274/pexels-photo-1181274.jpeg?auto=compress&cs=tinysrgb&w=600)",
+            backgroundImage: "url(https://images.pexels.com/photos/1181274/pexels-photo-1181274.jpeg)",
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
-            <div className="absolute inset-0 bg-black/40" />
+            <div className="absolute inset-0" />
           
           <div className="relative z-10 text-center space-y-8 px-4 max-w-4xl mx-auto">
             <h1 className="text-5xl md:text-7xl font-bold text-white">
