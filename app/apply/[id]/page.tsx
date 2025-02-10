76a7d0531bf210e78ed327af0204ec391216468e
harryyking <arthurharry06@gmail.com>
2025-02-10 17:57:13 +0000

    add apply

--- app/apply/[id]/page.tsx
+++ app/apply/[id]/page.tsx
@@ -20,12 +20,12 @@ const page = async({params}: Paramprops) => {
  
 
   return (
-    <div className='max-w-6xl mx-auto bg-gray-50'>
-      <h1>
+    <div className='max-w-6xl mx-auto bg-gray-50 min-h-screen'>
+      <h1 className='mb-8'>
       <span className='text-4xl font-semibold'>{jobId.companyName}</span> is hiring a {jobId.position}
       </h1>
 
-      <p>{jobId.description}</p>
+      <p className='prose'>{jobId.description}</p>
 
 
       <Button size={"lg"} className='mx-auto '>Apply for this job</Button>
